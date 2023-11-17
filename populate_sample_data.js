543258862341a30518cf7532e35f4d27daf0dc07
Megan Moisant <z1808084@students.niu.edu>
2023-11-17 14:44:30 -0600

    halp marceline plz

--- populate_sample_data.js
+++ populate_sample_data.js
@@ -3,12 +3,14 @@ const AppDAO = require('./models/app_dao')
 const LegacyDAO = require('./models/legacy_dao')
 const InventoryRepository = require('./models/inventory_repository')
 const ProductRepository = require('./models/product_repository')
+const OrderRepository = require('./models/order_repository')
 
 function main() {
     const legacyDao = new LegacyDAO()
     const dao = new AppDAO('./db/database.db')
     const inventoryRepo = new InventoryRepository(dao)
     const productRepo = new ProductRepository(legacyDao)
+    const orderRepo = new OrderRepository(dao)
 
     inventoryRepo.createTable()
         .then(() => productRepo.getAll())
@@ -25,6 +27,29 @@ function main() {
             return new Promise((resolve, reject) => {
                 rows.forEach((row) => {
                     console.log(`product_id = ${row.partNumber} quantity = ${row.quantity}`)
+                })
+                resolve('success')
+            })
+        })
+        .catch((err) => {
+            console.error(err.message)
+        })
+
+        orderRepo.createTable()
+        .then(() => orderRepo.getAll())
+        .then((rows) => {
+            return new Promise((resolve, reject) => {
+                rows.forEach((row) => {
+                    orderRepo.create("Megan", "Moisant", "megmoisant@gmail.com", 39.99, 20, "400 E Hillcrest Dr", "Dekalb", "IL", "60031", "United States")
+                })
+                resolve('success')
+            })
+        })
+        .then(() => orderRepo.getAll())
+        .then((rows) => {
+            return new Promise((resolve, reject) => {
+                rows.forEach((row) => {
+                    console.log(`date = ${row.date} order_id = ${row.id}`)
                 })
                 resolve('success')
             })
