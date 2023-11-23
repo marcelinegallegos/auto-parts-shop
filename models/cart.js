886c4908e37f7769e2701c7dc5dc4273d0fa8f3d
Kendra Ferguson <kendra.ferguson515@gmail.com>
2023-11-22 19:54:57 -0600

    Keeps track of cart, qty's, and total price

--- models/cart.js
+++ models/cart.js
@@ -1,32 +1,32 @@
+/**
+ * Shopping Cart Model
+ */
+
 let cart = null;
 module.exports = class Cart {
 
     static save(part) {
 
-        if (cart) { //if cart is not empty
-
+        //if cart is empty, initialize it
+        if (!cart) {    
+            cart = { parts: [], totalPrice: 0 };    
         }
-        else {  //if cart is currently empty
 
-            //initialize empty cart
-            cart = { parts: [], totalPrice: 0 };   
-            part.quantity = 1;
+        const price = part[0].price;
 
-            //add part to the array of parts
-            cart.parts.push(part);  
+        // check if the part is already in the cart
+        const existingPart = cart.parts.find(cartPart => cartPart.number === part[0].number);
 
-            const price = part[0].price;
-            console.log('Price:', price);
-
-            
-            cart.totalPrice = price;
-            
-            console.log("Part price:", part.price);
-            console.log("Part: ", part)
+        if (existingPart) {
+            existingPart.quantity += 1;
+        } else {
+            part[0].quantity = 1;
+            cart.parts.push(part[0]);
         }
+        cart.totalPrice += price;
     }
 
     static getCart() {
         return cart;
     }
-}
+};
