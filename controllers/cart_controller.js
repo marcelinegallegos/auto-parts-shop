8b20b116b5246445ed28ebd1d0e881a5e21c2389
Marceline Gallegos <marceline.gallegos@icloud.com>
2023-11-30 20:46:37 -0600

    Update routes

--- controllers/cart_controller.js
+++ controllers/cart_controller.js
@@ -10,6 +10,10 @@ const legacyDao = new LegacyDAO()
 const partRepo = new PartRepository(legacyDao)
 const inventoryRepo = new InventoryRepository(dao)
 
+exports.index = asyncHandler(async (req, res, next) => {
+    res.render('cart.ejs')
+})
+
 exports.addToCart = asyncHandler(async (req, res, next) => {
     const addedPart = await partRepo.getById(req.body.partNumber)
 
@@ -23,25 +27,21 @@ exports.addToCart = asyncHandler(async (req, res, next) => {
     }
 })
 
-exports.setInCartQuantity = asyncHandler(async (req, res, next) => {
-    const partNumber = req.body.partNumber
-    const quantity = req.body.quantity
-
-    Cart.setQuantity(partNumber, quantity)
+exports.updateQuantityInCart = asyncHandler(async (req, res, next) => {
+    Cart.setQuantity(req.body.partNumber, req.body.newQuantity)
     res.json({ message: 'Updated cart' })
 })
 
 exports.getCart = asyncHandler(async (req, res, next) => {
     let cart = Cart.getCart()
     for (part of cart.parts) {
-        part.inStockQuantity = (await inventoryRepo.getById(part.number)).quantity
+        part.quantityInStock = (await inventoryRepo.getById(part.number)).quantity
     }
-    res.render('cart.ejs', { cart: cart })
+    res.json(cart)    
 })
 
 exports.removeFromCart = asyncHandler(async (req, res, next) => {
     Cart.remove(req.body.productId)
-    res.redirect('/ShoppingCart')
 })
 
 exports.updateQuantity = asyncHandler(async (req, res, next) => {
