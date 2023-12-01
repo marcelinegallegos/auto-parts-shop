6a3f7bb581814316ef58c1d60625c17117ba14d1
Huajie Huang <z1975847@students.niu.edu>
2023-11-30 19:10:34 -0600

    Implement /get-update

--- routes/shipping_cost.js
+++ routes/shipping_cost.js
@@ -29,8 +29,6 @@ router.post('/add-cost', function(req, res) {
         });
 });
 
-
-
 // Route for removing a cost
 router.post('/remove-cost', function(req, res) {
     const id = req.body.id;
@@ -47,10 +45,11 @@ router.post('/remove-cost', function(req, res) {
         });
 });
 
+
 // Route for getting all costs
 router.get('/get-costs', function(req, res) {
+    
     // Get all costs from the database
-    //shippingRepo.getAll()
     shippingRepo.sort()
         .then((costs) => {
             // Send the costs back to the client
@@ -62,10 +61,47 @@ router.get('/get-costs', function(req, res) {
         });
 });
 
-router.post('/get-update', function(req,res){
+router.post('/get-update', async function (req, res) {
+    const maxWeight = parseInt(req.body.maxWeight);
+
+    try {
+        const costs = await shippingRepo.getAll();
+
+        let maxWeightFound = 0;
+        let nextCost = null;
+
+        // Looping to find the number that the user entered is within range
+        for (const cost of costs) {
+            if (cost.maxWeight < maxWeight && cost.maxWeight > maxWeightFound) {
+                maxWeightFound = cost.maxWeight;
+            }
+
+            // nextCost stores cost data so it's able to update minWeight
+            if (cost.minWeight < maxWeight && cost.maxWeight > maxWeight) {
+                nextCost = cost;
+            }
+            
+            // Check if entered weight matches either minWeight or maxWeight
+            if (cost.minWeight === maxWeight || cost.maxWeight === maxWeight) {
+                // If it matches, return without updating the database
+                res.json({ minWeight: cost.minWeight });
+                return;
+            }
+        }
+
+        const minWeight = maxWeightFound || 0;
+
+        if (nextCost) {
+            await shippingRepo.update(nextCost.id, maxWeight, nextCost.maxWeight, nextCost.cost);
+        }
+
+        res.json({ minWeight });
+    } catch (error) {
+        console.error('Error updating maxWeight:', error);
+        res.status(500).json({ message: 'Error updating maxWeight' });
+    }
+});
 
 
 
-    
-})
-module.exports = router
+module.exports = router;
