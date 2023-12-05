6f60e4efaf9f97c51748a9c513c32122e6f59262
Marceline Gallegos <marceline.gallegos@icloud.com>
2023-12-05 14:08:38 -0600

    Add email template

--- scripts/mail.js
+++ scripts/mail.js
@@ -1,9 +1,10 @@
-const nodemailer = require("nodemailer")
+const nodemailer = require('nodemailer')
+const hbs = require('nodemailer-express-handlebars')
 
 class Mail {
     constructor() {
         this.transporter = nodemailer.createTransport({
-            host: "smtp.gmail.com",
+            host: 'smtp.gmail.com',
             port: 465,
             secure: true,
             auth: {
@@ -11,18 +12,30 @@ class Mail {
                 pass: process.env.SMTP_PASS,
             },
         })
+        this.transporter.use('compile', hbs({
+            viewEngine: {
+                extname: '.hbs',
+                layoutsDir: './views/',
+                defaultLayout: false,
+                partialsDir: './views/partials/',
+            },
+            viewPath: './views/',
+            extName: '.hbs'
+        }))
     }
 
-    async sendShippingEmail(email) {
+    async sendShippingEmail(emailAddr, order, items) {
     const info = await this.transporter.sendMail({
-        from: "fa2023.csci467.group1a@gmail.com",
-        to: email,
-        subject: "Your order has shipped",
-        text: "Hello world?",
-        html: "<b>Hello world?</b>",
+        from: 'fa2023.csci467.group1a@gmail.com',
+        to: emailAddr,
+        subject: 'Your order has shipped',
+        template: 'email',
+        context: {
+            order : order, items : items
+        }
     })
 
-    console.log("Message sent: %s", info.messageId)
+    console.log('Message sent: %s', info.messageId)
     }
 }
 
